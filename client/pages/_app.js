60512ba0d69305db6332cd6baa0581168ee97b7f
Furkan ALAN <furkanalan@hotmail.com>
2022-08-05 21:16:57 +0300

    Added material-ui, Roboto font and material icons

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,8 @@
 import 'styles/globals.scss'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // Components
 import Layout from 'components/layout/Layout.js'
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,13 @@
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+
 // Components
 import { HeapversesOneWayRotation } from 'components/logoAnimations/LogoAnimations';
 
 export default function Home() {
   return (
     <div>
+      <ThreeDRotation fontSize="small" />
+      <ThreeDRotation fontSize="large" />
       <HeapversesOneWayRotation />
     </div>
   )
