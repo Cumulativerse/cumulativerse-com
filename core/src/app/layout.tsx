31370223d503a4aeaa1a13a32eae9ed8cdbae75d
Furkan Alan <furkanalan@hotmail.com>
2024-04-03 08:38:10 +0300

    removed old website and configured new one

--- core/src/app/layout.tsx
+++ core/src/app/layout.tsx
@@ -4,6 +4,16 @@ import Link from 'next/link';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import logo from '@/assets/images/logo.png';
+// Simple Icons
+import {
+  SiX,
+  SiInstagram,
+  SiDiscord,
+  SiMedium,
+  SiOpensea,
+  SiGithub,
+  SiMaildotru,
+} from '@icons-pack/react-simple-icons';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,32 +33,61 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dark">
-      <body
-        className={`${inter.className} min-h-screen`}
-      >
-        <div className="container flex min-h-screen flex-col items-center justify-between p-8">
+      <body className={`${inter.className} min-h-screen`}>
+        <div className="flex min-h-screen flex-col items-center justify-between p-8">
           {/* Header */}
           <Link href="/">
             <Image
               src={logo}
               alt="Logo"
-              className="h-10 w-fit rounded-xl"
+              className="h-12 w-fit rounded-xl"
               priority
             />
           </Link>
           {/* Main */}
-            {children}
+          {children}
           {/* Footer */}
-          <div>
-            <span>©{new Date().getFullYear()} — Made by </span>
-            <a
-              href="https://www.cumulativerse.com"
-              className="hover:text-accent underline"
-              target="_blank"
-              rel="external author"
-            >
-              Cumulativerse
-            </a>
+          <div className="grid w-full grid-cols-1 items-center justify-items-center gap-3 p-3 sm:grid-cols-2 lg:grid-cols-3">
+            <span>
+              IPFS{' '}
+              <Link
+                href="https://ipfs.io/ipfs/QmVmifbia6WpwYnnEiq6AA3KVwmHUiS6TvkeEpENAg8ex5"
+                className="hover:text-accent underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                link
+              </Link>{' '}
+              for first proposal
+            </span>
+            {/* prettier-ignore */}
+            <div className="flex space-x-2">
+              {/* @ts-ignore */}
+              <Link href="https://twitter.com/cumulativerse" target="_blank" rel="noreferrer" ><SiX /></Link>
+              {/* @ts-ignore */}
+              <Link href="https://www.instagram.com/cumulativerse/" target="_blank" rel="noreferrer" ><SiInstagram /></Link>
+              {/* @ts-ignore */}
+              <Link href="https://discord.com/invite/XJKdhfmD4t" target="_blank" rel="noreferrer" ><SiDiscord /></Link>
+              {/* @ts-ignore */}
+              <Link href="https://medium.com/@cumulativerse" target="_blank" rel="noreferrer" ><SiMedium /></Link>
+              {/* @ts-ignore */}
+              <Link href="https://opensea.io/collection/cumulativerse" target="_blank" rel="noreferrer" ><SiOpensea /></Link>
+              {/* @ts-ignore */}
+              <Link href="https://github.com/Cumulativerse" target="_blank" rel="noreferrer" ><SiGithub /></Link>
+              {/* @ts-ignore */}
+              <Link href="mail:social@cumulativerse.com" target="_blank" rel="noreferrer" ><SiMaildotru /></Link>
+            </div>
+            <span className="sm:col-span-2 lg:col-span-1">
+              ©{new Date().getFullYear()} — Made by{' '}
+              <a
+                href="https://www.cumulativerse.com"
+                className="hover:text-accent underline"
+                target="_blank"
+                rel="external author"
+              >
+                Cumulativerse
+              </a>
+            </span>
           </div>
         </div>
       </body>
--- core/src/app/page.tsx
+++ core/src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
@@ -7,8 +9,73 @@ export default function Home() {
         <h1>Projects</h1>
       </div>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <div className="card bg-base-300 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Bupo:</h2>
+            <p>Business Portal for companies</p>
+            <p>
+              Build over most popular open source ERP system:{' '}
+              <Link
+                href="https://www.odoo.com"
+                className="hover:text-accent underline"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Odoo
+              </Link>
+            </p>
+            <div className="card-actions justify-end">
+              <Link href={'https://bupo.app'} className="btn btn-primary">
+                Go to App
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-300 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">App Template:</h2>
+            <p>Javascript App template for fellow developers </p>
+            <p>
+              Use same code to write website, browser extension, mobile and
+              desktop app
+            </p>
+            <div className="card-actions justify-end">
+              <Link
+                href={'https://github.com/Cumulativerse/js-app-template'}
+                className="btn btn-primary"
+              >
+                Github
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-300 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Cupapp:</h2>
+            <p>Serverless utility app collection for users to use freely</p>
+            <p>Includes image processing, direct file sharing etc.</p>
+            <p>Can be used from browser, PWA, mobile app, desktop app</p>
+            <div className="card-actions justify-end">
+              <div className="btn btn-disabled">Web Coming Soon</div>
+              <Link
+                href={'https://github.com/Cumulativerse/cupapp'}
+                className="btn btn-primary"
+              >
+                Github
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-300 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Coming projects:</h2>
+            <p>1. Free to play game fully on blockchain with Godot</p>
+            <p>2. Decentralized Discord</p>
+            <p>3. Social Network on substrate blockchain</p>
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
       </div>
     </main>
   );
